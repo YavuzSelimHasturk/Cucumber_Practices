9641fd53cba9d2a35225f0f1d29ef52c7954847e
YavuzSelimHasturk <yavuzselimhasturk@gmail.com>
2023-02-08 11:14:48 +0300

    08.02.2023

---
+++ src/test/java/pages/AmazonPage.java
@@ -0,0 +1,40 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class AmazonPage {
+
+    public AmazonPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(id = "twotabsearchtextbox")
+    public WebElement searchBox;
+
+    @FindBy(xpath = "(//*[@class='a-section a-spacing-small a-spacing-top-small'])[1]")
+    public WebElement resultnumber;
+
+    @FindBy(xpath = "(//*[@class='rush-component'])[5]")
+    public WebElement ilkCikanAramaSonucu;
+
+    @FindBy(xpath = "(//*[@id='submit.buy-now-announce'])")
+    public WebElement buttonBuyNow;
+
+    @FindBy(xpath = "(//*[@id='productTitle'])[1]")
+    public WebElement urunAdi;
+
+    @FindBy(xpath = "(//*[@class='a-icon a-icon-star a-star-4-5'])[1]")
+    public WebElement ratingSection;
+
+    @FindBy(xpath = "(//*[@id='imgTagWrapperId'])[1]")
+    public WebElement urunPhoto;
+
+    @FindBy(xpath = "(//*[text()=' Details '])[4]")
+    public WebElement labelShippingDetails;
+
+    @FindBy(xpath = "(//*[@class='a-popover a-popover-no-header a-declarative a-arrow-bottom'])[1]")
+    public WebElement sectionShippingDetails;
+}
---
+++ src/test/java/stepdefinitions/AmazonStepDefinitions.java
@@ -0,0 +1,103 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import pages.AmazonPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class AmazonStepDefinitions {
+
+    AmazonPage amazonPage = new AmazonPage();
+    Actions actions = new Actions(Driver.getDriver());
+    @Given("Kullanici amazon anasayfaya gider")
+    public void kullanici_amazon_anasayfaya_gider() {
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+    }
+
+    @When("arama kutusuna {string} yazip aratir")
+    public void aramaKutusunaYazipAratir(String aranacakUrunIsmi) {
+        aranacakUrunIsmi = ConfigReader.getProperty("aranacakUrun");
+
+        amazonPage.searchBox.sendKeys(aranacakUrunIsmi+ Keys.ENTER);
+
+    }
+
+    @Then("Amazon anasayfada oldugunu test eder")
+    public void amazonAnasayfadaOldugunuTestEder() {
+        String expectedAmazonUrl = "amazon.com";
+        assertTrue(Driver.getDriver().getCurrentUrl().contains(expectedAmazonUrl));
+    }
+
+
+    @And("sayfayi kapatir")
+    public void sayfayiKapatir() {
+        Driver.quitDriver();
+    }
+
+    @Then("cikan ilk sonuca tiklar")
+    public void cikanIlkSonucaTiklar() {
+        amazonPage.ilkCikanAramaSonucu.click();
+    }
+
+
+    @And("Sonucun acildigini gorur ve {string} gorundugunu test eder")
+    public void AcildiginiGorurVeGorundugunuTestEder(String gorunenYazi) {
+        gorunenYazi = ConfigReader.getProperty("gorunenYazi");
+
+        assertEquals(gorunenYazi,amazonPage.buttonBuyNow.getText());
+
+    }
+
+    
+
+
+
+
+
+    @Given("cikan sonuc sayisinin {string} den fazla oldugunu test eder")
+    public void cikanSonucSayisininDenFazlaOldugunuTestEder(String sayi) {
+
+        int girilenSayi = Integer.parseInt(ConfigReader.getProperty("sayi"));
+        String[] sonucSayisi = amazonPage.resultnumber.getText().split(" ");
+        System.out.println(Arrays.toString(sonucSayisi));
+        int kalanSayi = Integer.parseInt(sonucSayisi[2]);
+
+        assertTrue(kalanSayi>girilenSayi);
+
+    }
+
+    @And("urunun adinin, ratinginin, urun fotosunun gorunur oldugunu test eder")
+    public void urununAdininRatingininUrunfotosununGorunurOldugunuTestEder() {
+        ReusableMethods.bekle(2);
+        Assert.assertTrue(amazonPage.urunAdi.isDisplayed());
+        ReusableMethods.bekle(2);
+        Assert.assertTrue(amazonPage.ratingSection.isDisplayed());
+        ReusableMethods.bekle(2);
+        Assert.assertTrue(amazonPage.urunPhoto.isDisplayed());
+
+    }
+
+    @And("acilan sayfada yer alan shipping details yazisina tiklar")
+    public void acilanSayfadaYerAlanShippingDetailsYazisinaTiklar() {
+        amazonPage.labelShippingDetails.click();
+        ReusableMethods.bekle(2);
+    }
+
+    @And("shipping Details bilgilerinin gorundugunu test eder")
+    public void shippingDetailsBilgilerininGorundugunuTestEder() {
+        assertTrue(amazonPage.sectionShippingDetails.isDisplayed());
+    }
+}
