2d591a7075243f623dcb5e39317e15dad222420b
YavuzSelimHasturk <yavuzselimhasturk@gmail.com>
2023-03-22 10:20:21 +0300

    22.03.2023 yavuz

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -3,66 +3,73 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 
 public class Driver {
-    /* Driver class'indan driver'i getDriver() ile kullaniyoruz
-       Sonradan projeye katilan insanlarin Driver class'indan obje olusturarak
-       driver kullanmaya calismalarini engellemek icin
-       Driver class'ini SINGLETON PATERN ile duzenleyebiliriz
-       Bunun icin Driver class'inin parametresiz constructor'ini olusturup
-       access modifier'ini PRIVATE yapmamiz yeterli olur.
+    /*
+        Driver claass'indan drigiver'i getDriver methodu ile kullaniyoruz
+        sonradan projeye katilan insanlarin Driver class'indan obje olusturarak
+        driver kullanmaya calismalarini engellemek icin
+
+        Driver class'ini SINGLEON PATTERN ile duzenleyebiliriz
+        Bunun icin Driver class'inin parametresiz consrustor'ini olusturup
+        access modifier'ini private yapmamiz yeterli olur
      */
 
+    //kimse bu class'dan obje uretemez
     private Driver(){
-    }
 
+    }
     static WebDriver driver;
 
-    public static WebDriver getDriver(){
+    static ChromeOptions ops = new ChromeOptions();
 
-        String browser= ConfigReader.getProperty("browser");
-        if(driver==null) {
-
-            switch (browser) {
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            switch (ConfigReader.getProperty("browser")) {
                 case "chrome":
+                    ops.addArguments("--remote-allow-origins=*");
                     WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
+                    driver = new ChromeDriver(ops);
+                    break;
+                case "safari":
+                    WebDriverManager.safaridriver().setup();
+                    driver = new SafariDriver();
                     break;
 
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
-                    driver=new FirefoxDriver();
+                    driver = new FirefoxDriver();
                     break;
-
-                case "safari" :
-                    WebDriverManager.safaridriver().setup();
-                    driver= new SafariDriver();
-                    break;
-
                 default:
+                    ops.addArguments("--remote-allow-origins=*");
                     WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
+                    driver = new ChromeDriver(ops);
+
+
             }
+
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         }
         return driver;
     }
-    public static void closeDriver(){
 
-        if (driver != null){
+    public static void closeDriver() {
+        if (driver != null) {
             driver.close();
-            driver=null;
+            driver = null;
         }
     }
-    public static void quitDriver(){
-        if (driver != null){
-            driver.quit();
-            driver=null;
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.close();
+            driver = null;
         }
     }
 }
